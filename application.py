bf10556a3068fb0f63cc4071ecb650dda64c37c8
jesuspi00001 <jesuspi00001@gmail.com>
2023-03-16 10:50:53 +0100

    Version 1.

--- application.py
+++ application.py
@@ -1,10 +1,30 @@
-from flask import Flask
 
-application = Flask(__name__)
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+app = Flask(__name__)
 
-@application.route('/',methods=['GET','POST'])
+
+@app.route('/')
 def index():
-    return "Flask application va bien."
+   print('Request for index page received')
+   return render_template('index.html')
 
-if __name__=="__main__":
-    application.run()
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+   app.run()
